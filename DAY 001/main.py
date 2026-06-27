755e56e638d8b7580009c69e3438a23a0cca4d6c
LONAH AMARO <lonahamaro@gmail.com>
2026-06-27 12:26:42 +0530

    Create main.py

---
+++ DAY 001/main.py
@@ -0,0 +1,11 @@
+# BAND NAME GENERATOR
+
+print("Welcome to the Band Name Generator.")
+
+city = input("what is the name of the city you grew up in?\n")
+
+pet = input("what is the name of your pet\n")
+
+print("your band name is!"+ city + ""  + pet)
+
+
